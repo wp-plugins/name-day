a34b84578c00ef7a9af7b39cc06ecef44aa9f815
thomaz <thomaz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-19 12:28:09 +0000

    update of url
    git-svn-id: https://plugins.svn.wordpress.org/name-day/trunk@207953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nameday.php
+++ nameday.php
@@ -4,7 +4,7 @@ Plugin Name: Name Day
 Description: Name Day, prints the name day (Swedish namnsdag). See the readme for how to configure.
 Version: 1.0.4
 Author: Thomas L
-Plugin URI: http://www.liajnad.se/nameday
+Plugin URI: http://dev.liajnad.se/nameday
 Author URI: http://www.liajnad.se
 */
 
